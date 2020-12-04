abe3bdc4d2a5747b743bfac2a6a6518aa49d761d
Lionel Quiambao <lionelquiambao@gmail.com>
2020-12-03 20:26:35 -0800

    Abstracted header to AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-header elevated>
+    <q-toolbar>
+      <q-btn
+        flat
+        dense
+        round
+        icon="menu"
+        aria-label="Menu"
+        @click="toggleDrawer"
+      />
+
+      <q-toolbar-title>
+        {{ title }}
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  setup () {
+    const title = 'lioncross.dev_';
+
+    function toggleDrawer() {
+      console.error('In toggleDrawer');
+    }
+
+    return {
+      title,
+      toggleDrawer
+    }
+  }
+})
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,23 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
+    <app-header></app-header>
 
     <q-drawer
       v-model="leftDrawerOpen"
@@ -47,7 +30,9 @@
 </template>
 
 <script lang="ts">
+import { defineComponent, ref } from '@vue/composition-api';
 import EssentialLink from 'components/EssentialLink.vue'
+import AppHeader from 'components/AppHeader.vue'
 
 const linksData = [
   {
@@ -94,11 +79,9 @@ const linksData = [
   }
 ];
 
-import { defineComponent, ref } from '@vue/composition-api';
-
 export default defineComponent({
   name: 'MainLayout',
-  components: { EssentialLink },
+  components: { AppHeader, EssentialLink },
   setup() {
     const leftDrawerOpen = ref(false);
     const essentialLinks = ref(linksData);
