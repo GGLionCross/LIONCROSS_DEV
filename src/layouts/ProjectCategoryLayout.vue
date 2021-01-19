8d7849c32aec78c5717998ced987f21706488add
Lionel Quiambao <lionelquiambao@gmail.com>
2021-01-18 21:00:17 -0800

    Renamed CategoryLayout to ProjectCategoryLayout

--- src/layouts/ProjectCategoryLayout.vue
+++ src/layouts/ProjectCategoryLayout.vue
@@ -1,21 +1,26 @@
 <template>
   <div>
-    <div class="category-name-container">
+    <div class="category-name-container q-px-lg q-py-md">
       {{ categoryName }}
+    </div>
+    <div class="q-px-lg q-py-md">
+      <slot></slot>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
+import ProjectCard from 'src/components/ProjectCard.vue';
 
 export default defineComponent({
-  name: 'CategoryLayout',
+  name: 'ProjectCategoryLayout',
+  components: { ProjectCard },
   props: {
     categoryName: {
       type: String,
       required: true
-    }
+    },
   }
 });
 </script>
@@ -24,6 +29,5 @@ export default defineComponent({
 .category-name-container {
   border-bottom: 2px solid white;
   font-size: 2em;
-  padding: 16px 32px;
 }
 </style>
