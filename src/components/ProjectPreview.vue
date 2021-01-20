97f3cd16a5aa712e9ba820fea5f7cfc0732af353
Lionel Quiambao <lionelquiambao@gmail.com>
2021-01-19 18:52:58 -0800

    Renamed ProjectCard to ProjectPreview

--- src/components/ProjectPreview.vue
+++ src/components/ProjectPreview.vue
@@ -1,10 +1,15 @@
 <template>
   <q-card class="project-card">
-    <q-img :src="thumbnailSrc" :ratio="16/9" />
+    <q-img class="cursor-pointer" :src="thumbnailSrc" :ratio="16/9" />
     <q-card-section class="text-accent">
-      <q-item-label class="project-title text-h6">
+      <q-item-label class="project-title text-h6 text-center cursor-pointer q-mb-sm">
         {{ projectTitle }}
       </q-item-label>
+      <div class="row justify-center">
+        <q-chip v-for="(tag, index) in projectTags" :key="index" size="sm" outline square>
+          {{ tag }}
+        </q-chip>
+      </div>
     </q-card-section>
   </q-card>
 </template>
@@ -22,6 +27,10 @@ export default defineComponent({
     projectTitle: {
       type: String,
       required: true
+    },
+    projectTags: {
+      type: Array,
+      default: () => []
     }
   },
   setup(props) {
--- src/pages/ProjectPage.vue
+++ src/pages/ProjectPage.vue
@@ -1,23 +1,23 @@
 <template>
   <q-page class="column justify-start">
     <project-list-layout category-name="Web Development">
-      <project-card
+      <project-preview
         project-name="statboosthq"
         project-title="statboosthq.com"
         :project-tags="statboostTags"
-      ></project-card>
+      ></project-preview>
     </project-list-layout>
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
-import ProjectCard from 'components/ProjectCard.vue';
+import ProjectPreview from 'components/ProjectPreview.vue';
 import ProjectListLayout from 'layouts/ProjectListLayout.vue';
 
 export default defineComponent({
   name: 'ProjectPage',
-  components: { ProjectCard, ProjectListLayout },
+  components: { ProjectPreview, ProjectListLayout },
   setup() {
     let statboostTags = ['Website Manager', 'Wix'];
     return {
