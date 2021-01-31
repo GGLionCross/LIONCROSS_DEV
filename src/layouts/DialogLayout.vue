eb5120c43305fa6fc2cebecfda03ed93133a47a2
Lionel Quiambao <lionelquiambao@gmail.com>
2021-01-30 18:46:17 -0800

    Created dialog layout component

---
+++ src/layouts/DialogLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-dialog v-bind="$attrs">
+    <q-layout>
+      <slot></slot>
+    </q-layout>
+  </q-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'DialogLayout'
+});
+</script>
