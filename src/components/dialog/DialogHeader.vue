bb4a5eabf71e20a3c83a6efd5c5f139dadd6454f
Lionel Quiambao <lionelquiambao@gmail.com>
2021-01-30 18:56:14 -0800

    Created the dialog header component

---
+++ src/components/dialog/DialogHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-header>
+    <slot></slot>
+  </q-header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+export default defineComponent({
+  name: 'DialogHeader'
+});
+</script>
