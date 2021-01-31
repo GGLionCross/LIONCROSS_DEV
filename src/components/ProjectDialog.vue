062f6c28bc3a584da3d30df8420470e0ba9b7425
Lionel Quiambao <lionelquiambao@gmail.com>
2021-01-30 18:49:39 -0800

    Created project dialog component

---
+++ src/components/ProjectDialog.vue
@@ -0,0 +1,19 @@
+<template>
+  <dialog-layout v-bind="$attrs">
+    
+  </dialog-layout>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api';
+import DialogLayout from 'layouts/DialogLayout.vue';
+
+export default defineComponent({
+  name: 'ProjectDialog',
+  components: { DialogLayout }
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
