21e44c13137d4044d0ba3c714fe140dd2c95fb72
Lionel Quiambao <lionelquiambao@gmail.com>
2021-02-01 22:59:47 -0800

    Created statboost dialog

---
+++ src/components/project/projects/ProjectStatboost.vue
@@ -0,0 +1,13 @@
+<template>
+  <project-dialog v-bind="$attrs"></project-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import ProjectDialog from 'components/project/ProjectDialog.vue';
+
+export default defineComponent({
+  name: 'ProjectStatboost',
+  components: { ProjectDialog }
+})
+</script>
