5ac0ea49a091dddfda1a85dedb042d0dbb8de121
Lionel Quiambao <lionelquiambao@gmail.com>
2021-02-01 22:33:03 -0800

    Created dialog header component, wip

--- src/components/dialog/DialogHeader.vue
+++ src/components/dialog/DialogHeader.vue
@@ -1,12 +1,22 @@
 <template>
-  <q-header>
-    <slot></slot>
+  <q-header class="bg-accent text-header">
+    <q-toolbar>
+      <q-toolbar-title>
+        {{ dialogTitle }}
+      </q-toolbar-title>
+    </q-toolbar>
   </q-header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
 export default defineComponent({
-  name: 'DialogHeader'
+  name: 'DialogHeader',
+  props: {
+    dialogTitle: {
+      type: String,
+      required: true
+    }
+  }
 });
 </script>
--- src/components/project/ProjectDialog.vue
+++ src/components/project/ProjectDialog.vue
@@ -1,19 +1,22 @@
 <template>
   <dialog-layout v-bind="$attrs">
-    
+    <dialog-header :dialog-title="projectTitle"></dialog-header>
   </dialog-layout>
 </template>
 
 <script>
 import { defineComponent } from '@vue/composition-api';
 import DialogLayout from 'layouts/DialogLayout.vue';
+import DialogHeader from 'components/dialog/DialogHeader.vue';
 
 export default defineComponent({
   name: 'ProjectDialog',
-  components: { DialogLayout }
+  components: { DialogLayout, DialogHeader },
+  props: {
+    projectTitle: {
+      type: String,
+      required: true
+    }
+  }
 });
 </script>
-
-<style lang="scss" scoped>
-
-</style>
