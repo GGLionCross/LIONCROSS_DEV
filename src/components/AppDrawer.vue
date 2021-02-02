c4555090de002dc47538cabeaff87a72140416c9
Lionel Quiambao <lionelquiambao@gmail.com>
2021-02-01 18:38:49 -0800

    Fixed drawer visible state not getting changed on drawer slide close

--- src/components/AppDrawer.vue
+++ src/components/AppDrawer.vue
@@ -3,22 +3,30 @@
     :value="visible"
     show-if-above
     content-class="bg-secondary"
+    @input="setDrawerVisible"
   >
   </q-drawer>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
-import { useGetters } from 'vuex-composition-helpers';
+import { useGetters, useMutations } from 'vuex-composition-helpers';
 import { GET_DRAWER_VISIBLE } from 'src/store/types/getterTypes';
+import { SET_DRAWER_VISIBLE } from 'src/store/types/mutationTypes';
 
 export default defineComponent({
   name: 'AppDrawer',
   setup () {
     const getters = useGetters([GET_DRAWER_VISIBLE]);
     const visible = getters[GET_DRAWER_VISIBLE];
+
+    const mutations = useMutations([SET_DRAWER_VISIBLE]);
+    function setDrawerVisible(value: boolean) {
+      mutations[SET_DRAWER_VISIBLE](value);
+    }
     return {
-      visible
+      visible,
+      setDrawerVisible
     }
   }
 })
