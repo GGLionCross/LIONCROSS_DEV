b787e7155329e7d9f2aad56d3a96fee8314f90bf
Lionel Quiambao <lionelquiambao@gmail.com>
2021-02-03 01:07:35 -0800

    Created dialog content component

---
+++ src/components/dialog/DialogContent.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page-container>
+    <q-page padding>
+      <slot></slot>
+    </q-page>
+  </q-page-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'DialogContent',
+  setup () {
+    
+    return {}
+  }
+});
+</script>
