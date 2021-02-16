fa1afe8b54b058a387cfef3fa6d6338dd915f0d8
Lionel Quiambao <lionelquiambao@gmail.com>
2021-02-15 21:35:39 -0800

    Added highlight sources to statboost config

--- src/config/projects/web-development.ts
+++ src/config/projects/web-development.ts
@@ -6,6 +6,14 @@ export default {
     tags: ['Website Manager', 'Wix'],
     previewSrc: '/portfolio/statboosthq/thumbnail.png',
     dialogComponent: () =>
-      import('components/project/dialogs/StatboostDialog.vue')
+      import('components/project/dialogs/StatboostDialog.vue'),
+    highlightSrcs: [
+      '/portfolio/statboosthq/highlights/home-1.png',
+      '/portfolio/statboosthq/highlights/home-2.png',
+      '/portfolio/statboosthq/highlights/home-3.png',
+      '/portfolio/statboosthq/highlights/home-4.png',
+      '/portfolio/statboosthq/highlights/shop.png',
+      '/portfolio/statboosthq/highlights/contact.png',
+    ]
   }
 };
