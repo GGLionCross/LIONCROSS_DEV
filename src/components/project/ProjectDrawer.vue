ba8b39f314a4f4b65114d842c631517f5e284b04
Lionel Quiambao <lionelquiambao@gmail.com>
2021-04-06 19:58:36 -0700

    Created skeleton for Project Drawer

---
+++ src/components/project/ProjectDrawer.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-drawer v-bind="allAttrs" v-on="listeners">
+  </q-drawer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  setup (_, { attrs, listeners}) {
+    let allAttrs = {
+      contentClass: "bg-secondary",
+      ...attrs
+    }
+    
+    return {
+      allAttrs,
+      listeners
+    }
+  }
+});
+</script>
